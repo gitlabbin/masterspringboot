0e8396467a82863b52b490660dab089389a4f034
Brian Chen <brian.chen20@det.nsw.edu.au>
2020-04-25 21:32:46 +1000

    cl

--- demo-docker/src/main/java/com/example/demodocker/config/Swagger2Config.java
+++ demo-docker/src/main/java/com/example/demodocker/config/Swagger2Config.java
@@ -15,41 +15,42 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class Swagger2Config {
-  private final String BASE_PATH = "/idm";
+    private final String BASE_PATH = "/idm";
 
-  @Bean
-  public Docket api() {
-    return new Docket(DocumentationType.SWAGGER_2).pathProvider(new PathProvider() {
-      @Override
-      public String getApplicationBasePath() {
-        return null;
-      }
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).pathProvider(new PathProvider() {
+            @Override
+            public String getApplicationBasePath() {
+                return null;
+            }
 
-      @Override
-      public String getOperationPath(String operationPath) {
-        return BASE_PATH.concat(operationPath);
-      }
-      @Override
-      public String getResourceListingPath(String groupName, String apiDeclaration) {
-        return BASE_PATH;
-      }
-    })
-        .select()
-        .apis(RequestHandlerSelectors.basePackage("com.example.demodocker"))
-        .paths(PathSelectors.regex("/api/v1/.*"))
-        .build()
-        .apiInfo(apiEndPointsInfo());
-  }
+            @Override
+            public String getOperationPath(String operationPath) {
+                return BASE_PATH.concat(operationPath);
+            }
 
-  private ApiInfo apiEndPointsInfo() {
+            @Override
+            public String getResourceListingPath(String groupName, String apiDeclaration) {
+                return BASE_PATH;
+            }
+        })
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.demodocker"))
+                .paths(PathSelectors.regex("/api/v1/.*"))
+                .build()
+                .apiInfo(apiEndPointsInfo());
+    }
 
-    return new ApiInfoBuilder()
-        .title("IDM Consumer REST API")
-        .description("IDM Management REST API")
-        .contact(new Contact("Api support", "www.idmservice.com", "idmsupport@gmail.com"))
-        .license("Apache 2.0")
-        .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
-        .version("1.0.0")
-        .build();
-  }
+    private ApiInfo apiEndPointsInfo() {
+
+        return new ApiInfoBuilder()
+                .title("IDM Consumer REST API")
+                .description("IDM Management REST API")
+                .contact(new Contact("Api support", "www.idmservice.com", "idmsupport@gmail.com"))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.0")
+                .build();
+    }
 }
