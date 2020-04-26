503018a5f316d11cce57e3f0735e05d7f9e42acc
Brian Chen <brian.chen20@det.nsw.edu.au>
2020-04-26 11:48:14 +1000

    more actions

--- demo-docker/src/main/java/com/example/demodocker/controller/DemoController.java
+++ demo-docker/src/main/java/com/example/demodocker/controller/DemoController.java
@@ -4,9 +4,7 @@ import com.example.demodocker.model.Customer;
 import io.swagger.annotations.*;
 import java.util.ArrayList;
 import java.util.List;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -34,5 +32,41 @@ public class DemoController {
     customerList.add(new Customer(1, "frank"));
     customerList.add(new Customer(2, "john"));
     return customerList;
+  }
+
+  @ApiOperation(
+      value = "Add new customer",
+      response = Customer.class,
+      authorizations = {@Authorization(value = "apiKey")})
+  @ApiResponses(
+      value = {
+        @ApiResponse(code = 200, message = "Successfully retrieved list"),
+        @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+        @ApiResponse(
+            code = 403,
+            message = "Accessing the resource you were trying to reach is forbidden"),
+        @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+      })
+  @PostMapping("/customers")
+  public Customer addCustomer() {
+    return new Customer(3, "Albert");
+  }
+
+  @ApiOperation(
+      value = "Delete customer",
+      response = String.class,
+      authorizations = {@Authorization(value = "apiKey")})
+  @ApiResponses(
+      value = {
+        @ApiResponse(code = 200, message = "Successfully retrieved list"),
+        @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+        @ApiResponse(
+            code = 403,
+            message = "Accessing the resource you were trying to reach is forbidden"),
+        @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+      })
+  @DeleteMapping("/customers")
+  public String deleteCustomer() {
+    return "Albert";
   }
 }
